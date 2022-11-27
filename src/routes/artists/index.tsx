7059ba5cb1954315c67cdefd187da623d94118ed
jesusbarcam <jesusbarcam@gmail.com>
2022-11-27 19:54:16 +0100

    First commit

---
+++ src/routes/artists/index.tsx
@@ -0,0 +1,16 @@
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
+import styles from "./artists.css?inline";
+
+export default component$(() => {
+  useStylesScoped$(styles);
+  return (
+    <div class="pruebas">
+      <p>Hola aqui mostraremos a todos los artistas</p>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Artists",
+};
---
+++ src/routes/layout.tsx
@@ -0,0 +1,20 @@
+import { component$, Fragment, Slot } from "@builder.io/qwik";
+import Header from "../components/header/header";
+
+export default component$(() => {
+  return (
+    <Fragment>
+      <main>
+        <Header />
+        <section>
+          <Slot />
+        </section>
+      </main>
+      <footer>
+        <a href="https://www.builder.io/" target="_blank">
+          Made with ♡ by Builder.io and Jesus Barcam
+        </a>
+      </footer>
+    </Fragment>
+  );
+});
